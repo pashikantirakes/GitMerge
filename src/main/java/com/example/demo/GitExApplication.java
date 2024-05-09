02413208b45da8f21e5a6014544904a3b33c7569
pashikantirakes <rakeshpashikanti740@gmail.com>
2024-05-09 21:43:45 +0530

    added url

--- src/main/java/com/example/demo/GitExApplication.java
+++ src/main/java/com/example/demo/GitExApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitExApplication {
 
+private static final Stringurl="http://localhost:8080/web";
 	public static void main(String[] args) {
 		SpringApplication.run(GitExApplication.class, args);
 	}
