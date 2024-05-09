7d716402bb53a2cc3833ccbe942fa6dc2de84eab
pashikantirakes <rakeshpashikanti740@gmail.com>
2024-05-09 21:50:11 +0530

    feature1 changes

--- src/main/java/com/example/demo/GitExApplication.java
+++ src/main/java/com/example/demo/GitExApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitExApplication {
-
+   static String s="hey";
 	public static void main(String[] args) {
 		SpringApplication.run(GitExApplication.class, args);
 	}
